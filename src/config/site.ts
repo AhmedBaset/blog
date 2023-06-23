05a90bcf5f43ade35100c3f4852ecfc56b6b2979
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-23 20:44:03 +0000

    add navLinks and links

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,3 +1,16 @@
+import {
+	BadgeIcon,
+	BookCopyIcon,
+	CodepenIcon,
+	FacebookIcon,
+	GithubIcon,
+	HomeIcon,
+	LinkedinIcon,
+	TwitterIcon,
+	UserIcon,
+	YoutubeIcon,
+} from "lucide-react"
+
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
@@ -8,7 +21,58 @@ export const siteConfig = {
 	description:
 		"Next.js starter template with TypseScript, ESLint, Prettier, Tailwind CSS, and Shadcn/ui",
 	image: "https://github.com/A7med3bdulBaset.png",
-	links: {
-		github: "https://github.com/A7med3bdulBaset",
-	},
+	links: [
+		{
+			label: "Github",
+			href: "https://github.com/A7med3bdulBaset",
+			Icon: GithubIcon,
+		},
+		{
+			label: "Twitter",
+			href: "https://twitter.com/A7med3bdulBaset",
+			Icon: TwitterIcon,
+		},
+		{
+			label: "LinkedIn",
+			href: "https://www.linkedin.com/in/A7med3bdulBaset/",
+			Icon: LinkedinIcon,
+		},
+		{
+			label: "Youtube",
+			href: "https://www.youtube.com/A7med3bdulBaset",
+			Icon: YoutubeIcon,
+		},
+		{
+			label: "CodePen",
+			href: "https://codepen.io/A7med3bdulBaset",
+			Icon: CodepenIcon,
+		},
+		{
+			label: "Dev.to",
+			href: "https://dev.to/A7med3bdulBaset",
+			Icon: BookCopyIcon,
+		},
+		{
+			label: "Facebook",
+			href: "https://www.facebook.com/A7med3bdulBaset",
+			Icon: FacebookIcon,
+		},
+	],
+	navLinks: [
+		{
+			name: "Home",
+			href: "/",
+			Icon: HomeIcon,
+		},
+		{
+			name: "About",
+			href: "/about",
+			Icon: UserIcon,
+		},
+		{
+			name: "Badges",
+			href: "/badges",
+			Icon: BadgeIcon,
+		},
+	],
 }
