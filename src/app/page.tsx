8b4c08069f8187cc33d63e8dd7175c6aaac4ba0f
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-24 10:29:55 +0000

    Fix all ci errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,13 @@ export default function Index() {
 		compareDesc(new Date(a.date), new Date(b.date))
 	)
 
-	
 	return (
-		<div className="py-8 max-w-6xl mx-auto container">
+		<div className="container mx-auto max-w-6xl py-8">
 			<h1 className="mb-8 text-center text-2xl font-black">
 				Welcome to my blog!
 			</h1>
-			
-			<div className="grid grid-cols-auto gap-3">
+
+			<div className="grid-cols-auto grid gap-3">
 				{posts.map((post, idx) => (
 					<PostCard key={idx} {...post} />
 				))}
