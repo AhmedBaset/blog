02924bca2f133287e5dbf3aff28ef0f243c37c5a
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-24 10:08:06 +0000

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-export default function IndexPage() {
-	return <main className="container py-4">
-		<h1>Hello World</h1>
-	</main>
+import { allPosts } from "contentlayer/generated"
+import { compareDesc } from "date-fns"
+
+import PostCard from "@/components/PostCard"
+
+export default function Index() {
+	const posts = allPosts.sort((a, b) =>
+		compareDesc(new Date(a.date), new Date(b.date))
+	)
+
+	
+	return (
+		<div className="py-8 max-w-6xl mx-auto container">
+			<h1 className="mb-8 text-center text-2xl font-black">
+				Welcome to my blog!
+			</h1>
+			
+			<div className="grid grid-cols-auto gap-3">
+				{posts.map((post, idx) => (
+					<PostCard key={idx} {...post} />
+				))}
+			</div>
+		</div>
+	)
 }
