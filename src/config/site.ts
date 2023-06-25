ab11827d0f187c96935d5d8919217a385f80b28c
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 09:17:11 +0000

    feet: add alert-dialog from shadcn

--- src/config/site.ts
+++ src/config/site.ts
@@ -16,10 +16,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
 	name: "Ahmed AbdulBaset",
 	bio: "Full Stack Developer enjoying building things with Next.js",
-
-	title: "Next.js Starter Template",
-	description:
-		"Next.js starter template with TypseScript, ESLint, Prettier, Tailwind CSS, and Shadcn/ui",
+	username: "A7med3bdulBaset",
 	image: "https://github.com/A7med3bdulBaset.png",
 	links: [
 		{
