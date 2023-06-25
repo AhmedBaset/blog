46539a03d67acb0b340c82605d1a5dcb1402b817
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 09:16:36 +0000

    improve footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { siteConfig } from "@/config/site"
 
 function Footer() {
 	return (
-		<footer className="space-y-2 bg-slate-50 p-4 py-8 text-center text-sm dark:bg-slate-800">
+		<footer className="space-y-2 bg-sky-200 p-4 py-8 text-center text-sm dark:bg-slate-900">
 			<p className="text-slate-700 dark:text-slate-300">
 				&copy; 2023 - All rights reserved. <br />
 				Developed with <span className="text-rose-500">❤</span> by{" "}
