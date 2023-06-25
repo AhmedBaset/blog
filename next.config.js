57326d7111ba83b8a5481b9214811446206de6f3
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 09:12:20 +0000

    Configuration

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-      domains: ['picsum.photos', "github.com"
-   ]
-   }
+	images: {
+		domains: ["picsum.photos", "github.com", "localhost"],
+	},
 }
 
 module.exports = withContentlayer(nextConfig)
