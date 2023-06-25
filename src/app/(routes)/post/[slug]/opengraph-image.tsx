f31ef859cbe45478ee7c982dd951d198f603c783
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 09:53:08 +0000

    fix: opengraph-image.tsx

---
+++ src/app/(routes)/post/[slug]/opengraph-image.tsx
@@ -0,0 +1,134 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "@vercel/og";
+import { getPost } from "./page";
+
+
+
+
+
+export const runtime = "edge"
+
+const montserratRegular = fetch(
+	new URL("./../../../../assets/fonts/Montserrat-Regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer())
+
+const montserratExtraBold = fetch(
+	new URL(
+		"./../../../../assets/fonts/Montserrat-ExtraBold.ttf",
+		import.meta.url
+	)
+).then((res) => res.arrayBuffer())
+
+interface Props {
+	params: {
+		slug: string
+	}
+}
+
+export default async function og({ params: {slug} }: Props) {
+	try {
+		const fontRegular = await montserratRegular
+		const fontBold = await montserratExtraBold
+
+		const values = getPost(slug)
+
+		values.title =
+			values.title.length > 100
+				? values.title.slice(0, 100) + "..."
+				: values.title
+
+		return new ImageResponse(
+			(
+				<div
+					style={{
+						width: "100%",
+						height: "100%",
+						backgroundImage: `linear-gradient(45deg, #0b1120, #0d3256)`,
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						flexDirection: "column",
+					}}
+				>
+					<h2
+						key="title"
+						style={{
+							color: "white",
+							fontSize: "3rem",
+							fontFamily: "font",
+							fontWeight: "800",
+							width: "80%",
+						}}
+					>
+						{values.title}
+					</h2>
+					<p
+						key="description"
+						style={{
+							color: "#ccc",
+							fontSize: "1rem",
+							fontFamily: "font",
+							fontWeight: "400",
+							width: "80%",
+							lineHeight: "1.5rem",
+							lineClamp: "2rem",
+						}}
+					>
+						{values.description}
+					</p>
+					<div
+						key="link-block"
+						style={{
+							maxWidth: "80%",
+							height: "2.5rem",
+							backgroundColor: "#133c7f",
+							color: "#fff",
+							display: "flex",
+							justifyContent: "flex-start",
+							alignItems: "center",
+							borderRadius: "0.35rem",
+						}}
+					>
+						<span
+							key="space"
+							style={{ width: "1rem", whiteSpace: "nowrap" }}
+						>
+							{" "}
+						</span>
+						<span key="link" style={{ fontSize: "1rem" }}>
+							{process.env.NEXT_PUBLIC_URL}/{values.url}
+						</span>
+						<span
+							key="space-2"
+							style={{ width: "1rem", whiteSpace: "nowrap" }}
+						>
+							{" "}
+						</span>
+					</div>
+				</div>
+			),
+			{
+				width: 800,
+				height: 450,
+				fonts: [
+					{
+						name: "font",
+						data: fontRegular,
+						weight: 400,
+						style: "normal",
+					},
+					{
+						name: "font",
+						data: fontBold,
+						weight: 700,
+						style: "normal",
+					},
+				],
+			}
+		)
+	} catch (error) {
+		return new Response(`Failed to generate image`, {
+			status: 500,
+		})
+	}
+}
