45a2f3d3aa9ecff3d58697232328f805fcf965a4
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 14:24:18 +0000

    10.6

--- src/lib/getImage.ts
+++ src/lib/getImage.ts
@@ -3,9 +3,9 @@ import { Post } from "contentlayer/generated"
 export function getImage({ title, description, url }: Partial<Post>) {
 	const APP_URL = process.env.NEXT_PUBLIC_URL
 	const ogUrl = new URL(`/api/og`, APP_URL)
-	ogUrl.searchParams.set("title", title ?? "")
-	ogUrl.searchParams.set("description", description ?? "")
-	ogUrl.searchParams.set("url", url ?? "")
+	ogUrl.searchParams.set("title", normalize(title ?? ""))
+	ogUrl.searchParams.set("description", normalize(description ?? ""))
+	ogUrl.searchParams.set("url", normalize(url ?? ""))
 
 	
 
