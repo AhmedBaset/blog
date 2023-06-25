6c8775ab9dd965bce44767a5a03cc89154e11efa
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 13:26:14 +0000

    10.1

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 
 import Image from "next/image"
 import Link from "next/link"
@@ -20,7 +20,7 @@ export default async function PostCard({ title, description, url }: Post) {
 			<Card>
 				<CardContent className="p-0">
 					<Image
-						src={"/api/og"}
+						src={`${process.env.NEXT_PUBLIC_URL}/api/og`}
 						// src={getImage({ title, description, url })}
 						alt={title}
 						className="aspect-video w-full rounded-lg"
