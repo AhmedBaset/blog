976210cb7c5f3eb8f0a23525da8a075e044dfe1b
Ahmed Abdelbaset <A7med3bdulBaset@gmail.com>
2023-06-25 09:16:47 +0000

    improve: header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,4 @@
-import { UserPlus } from "lucide-react"
-
-import { Button } from "@shadcn/button"
-
+import Follow from "./Follow"
 import Logo from "./Logo"
 import MobileNavMenu from "./MobileNavMenu"
 import NavOptions from "./NavOptions"
@@ -10,7 +7,7 @@ import TabsBar from "./TabsBar"
 
 function Header() {
 	return (
-		<header className="container rounded-b-xl bg-slate-100 shadow-md shadow-slate-500/20 ring-1 ring-slate-500/20 dark:bg-slate-900">
+		<header className="container rounded-b-xl bg-sky-100 shadow-lg shadow-secondary ring-1 ring-slate-500/20 dark:bg-slate-900">
 			{/* Nav */}
 			<div className="flex-center justify-between gap-4 py-4">
 				<Logo className="hidden md:flex" /> {/* Desktop Only */}
@@ -22,10 +19,7 @@ function Header() {
 				<Logo className="flex md:hidden" /> {/* Mobile Only */}
 				{/* Socials Media */}
 				<SocialLinks className="md:order-2" />
-				<Button className="space-x-2 rounded-full">
-					<UserPlus />
-					<span>Follow</span>
-				</Button>
+				<Follow />
 			</div>
 			<div className="flex-center px-4">
 				<TabsBar />
